db0ece1c300708a08e053b72807d0567a661eb8c
JP <james@mail.com>
2025-01-16 22:07:07 +0000

    WIP begin adding behaviour to individual nodes

--- IsingMarket/Domain/Node.cs
+++ IsingMarket/Domain/Node.cs
@@ -5,11 +5,18 @@ public class Node
     /// <summary>
     /// Represents the spin state of this node, often +1 or -1.
     /// </summary>
-    public int Spin { get; set; }
+    public int StockLevel { get; set; }
+    public int MaxStockLevel { get; set; }
+    public int MinStockLevel { get; set; }
+    public int BuyValue { get; set; }
+    public int SellValue { get; set; }
+    public int Cash {  get; set; }
 
-    public Node(int initialSpin)
+    public Node(int stock, int buyValue, int sellValue)
     {
-        Spin = initialSpin;
+        StockLevel = stock;
+        BuyValue = buyValue;
+        SellValue = sellValue;
     }
 }
 
